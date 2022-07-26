6bff1bcaffe9ff69ade64f26c2ad2d6e08ff1344
realpissriolu <110011242+realpissriolu@users.noreply.github.com>
2022-07-25 22:12:08 -0700

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,13 @@
+// REAL math adder.
+
+#include <iostream>
+
+int main() {
+	int x, y;
+	std::cout << "enter a number\n";
+	std::cin >> x;
+	std::cout << "enter 2nd number\n";
+	std::cin >> y;
+	int z = x + y;
+	std::cout << "result: " << z;
+}
